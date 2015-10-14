73054d65e3d600752b234d5adcd6ee915416a248
Andyif <andjey1990@gmail.com>
2015-10-14 23:10:43 +0300

    updated test framework

--- src/test/java/GetGold.java
+++ src/test/java/GetGold.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
@@ -18,21 +19,26 @@ public class GetGold {
         webDriver.get(aces_raffle);
     }
 
-    private boolean raffleClosed(){
+    private boolean raffleClosed() throws InterruptedException {
+        webDriver.findElement(By.xpath("/html/body/div[3]/div[1]/legend/ol/li[2]/a")).click();
+        Thread.sleep(5000);
         if(webDriver.findElements(By.xpath("/html/body/div[3]/div[1]/div[2]/table/tbody[2]/tr/td[1]/a[1]")).size() == 0){
             return true;
         }
         return false;
     }
-    private boolean isAnyActiveRaffle(){
-        webDriver.findElement(By.linkText("Розыгрыши")).click();
+    private boolean isAnyActiveRaffle() throws InterruptedException {
+        webDriver.findElement(By.xpath("/html/body/div[3]/div[1]/legend/ol/li[2]/a")).click();
+        Thread.sleep(5000);
         if(webDriver.findElements(By.xpath("/html/body/div[3]/div[1]/div[2]/table/tbody[2]/tr/td[1]/a[1]")).size() != 0){
+            System.out.println("active presents");
             return true;
         }
         return false;
     }
-    private void enterName(){
+    private void enterName() throws InterruptedException {
         webDriver.findElement(By.xpath("/html/body/div[3]/div[1]/div[2]/table/tbody[2]/tr/td[1]/a[1]")).click();
+        Thread.sleep(5000);
         webDriver.findElement(By.name("drawing_input_login")).sendKeys("Andy_if");
         webDriver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div[1]/div/div[2]/button")).click();
     }
@@ -46,7 +52,9 @@ public class GetGold {
             while (!isAnyActiveRaffle()){
                 Thread.sleep(sleep_time);
             }
+            System.out.println("before name");
             enterName();
+            System.out.println("after name");
             Thread.sleep(5000);
             System.out.println(i);
             i++;
