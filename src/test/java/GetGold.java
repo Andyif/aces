2eea7b5077cf95aa74ad517d6004282dee7d7181
amayd <andjey1990@gmailcom>
2015-12-08 11:20:17 +0000

    test fixed

--- src/test/java/GetGold.java
+++ src/test/java/GetGold.java
@@ -11,7 +11,7 @@ import java.util.NoSuchElementException;
 
 public class GetGold {
     private WebDriver webDriver;
-    private final String aces_raffle = "http://aces.gg/index.php?do=streams&act=drawings";
+    private final String aces_raffle = "http://aces.gg";
 
     @BeforeClass
     public void setUp(){
@@ -20,27 +20,33 @@ public class GetGold {
     }
 
     private boolean raffleClosed() throws InterruptedException {
-        webDriver.findElement(By.xpath("/html/body/div[3]/div[1]/legend/ol/li[2]/a")).click();
+        webDriver.findElement(By.xpath("//*[@id='chatpanel']/ul/li[3]/a")).click();
         Thread.sleep(5000);
-        if(webDriver.findElements(By.xpath("/html/body/div[3]/div[1]/div[2]/table/tbody[2]/tr/td[1]/a[1]")).size() == 0){
+        System.out.println("check if closed");
+        System.out.println(webDriver.findElements(By.name("drawing_input_login")).size());
+        if(webDriver.findElements(By.name("drawing_input_login")).size() == 0){
+            System.out.println("closed");
             return true;
         }
         return false;
     }
     private boolean isAnyActiveRaffle() throws InterruptedException {
-        webDriver.findElement(By.xpath("/html/body/div[3]/div[1]/legend/ol/li[2]/a")).click();
+        webDriver.findElement(By.xpath("//*[@id='chatpanel']/ul/li[3]/a")).click();
         Thread.sleep(5000);
-        if(webDriver.findElements(By.xpath("/html/body/div[3]/div[1]/div[2]/table/tbody[2]/tr/td[1]/a[1]")).size() != 0){
+        System.out.println("check if active");
+        if(webDriver.findElements(By.name("drawing_input_login")).size() != 0){
             System.out.println("active presents");
             return true;
         }
         return false;
     }
     private void enterName() throws InterruptedException {
-        webDriver.findElement(By.xpath("/html/body/div[3]/div[1]/div[2]/table/tbody[2]/tr/td[1]/a[1]")).click();
+        //webDriver.findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/table/tbody[2]/tr[1]/td[1]/a[1]")).click();
         Thread.sleep(5000);
-        webDriver.findElement(By.name("drawing_input_login")).sendKeys("Andy_if");
-        webDriver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div[1]/div/div[2]/button")).click();
+        if(webDriver.findElements(By.name("drawing_input_login")).size() != 0){
+            webDriver.findElement(By.name("drawing_input_login")).sendKeys("Andy_if");
+            webDriver.findElement(By.xpath("//*[@id='tab_chatquizes']/div/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/div[2]/button")).click();
+        }
     }
 
     @Test
