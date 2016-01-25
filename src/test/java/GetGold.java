cd5bb84b1edffb323cc4b3e989a6a163241be109
amayd <andjey1990@gmailcom>
2016-01-25 15:04:09 +0000

    added win rate

--- src/test/java/GetGold.java
+++ src/test/java/GetGold.java
@@ -1,17 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import java.util.NoSuchElementException;
 
 public class GetGold {
     private WebDriver webDriver;
     private final String aces_raffle = "http://aces.gg";
+    private String name = "Andy_if";
+    int winQuantity = 0;
 
     @BeforeClass
     public void setUp(){
@@ -40,33 +37,48 @@ public class GetGold {
         }
         return false;
     }
-    private void enterName() throws InterruptedException {
+    private void enterName(final String name) throws InterruptedException {
         //webDriver.findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/table/tbody[2]/tr[1]/td[1]/a[1]")).click();
         Thread.sleep(5000);
         if(webDriver.findElements(By.name("drawing_input_login")).size() != 0){
-            webDriver.findElement(By.name("drawing_input_login")).sendKeys("Andy_if");
+            webDriver.findElement(By.name("drawing_input_login")).sendKeys(name);
             webDriver.findElement(By.xpath("//*[@id='tab_chatquizes']/div/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/div[2]/button")).click();
         }
     }
 
+    private void checkIfUserWon(final String name) {
+        String xpathList = "//*[@id=\"tab_chatquizes\"]/div/div[2]/div/div/div[2]/div[1]/div/div/div[2]/table/tbody[2]/tr";
+        String xpathUser = "//*[@id=\"tab_chatquizes\"]/div/div[2]/div/div/div[2]/div[1]/div/div/div[2]/table/tbody[2]/tr[%s]/td[2]/span";
+        int winnersQuantity = webDriver.findElements(By.xpath(xpathList)).size();
+        for (int i = 1; i < (winnersQuantity + 1); i++) {
+            String winnerName = webDriver.findElement(By.xpath(String.format(xpathUser, i))).getText();
+            System.out.println(winnerName);
+            if(name.equals(winnerName)){
+                winQuantity++;
+            }
+        }
+        System.out.println("Quantity of wins - " + winQuantity);
+    }
+
     @Test
     public void gettingGold() throws InterruptedException {
-        long sleep_time = 300000;
-        int i = 1;
+        long sleep_time = 60000;
+        int attempt = 1;
 
         while (true){
             while (!isAnyActiveRaffle()){
                 Thread.sleep(sleep_time);
             }
             System.out.println("before name");
-            enterName();
+            enterName(name);
             System.out.println("after name");
             Thread.sleep(5000);
-            System.out.println(i);
-            i++;
+            System.out.println("Attempt - " + attempt);
+            attempt++;
             while (!raffleClosed()){
                 Thread.sleep(sleep_time);
             }
+            checkIfUserWon(name);
         }
     }
 }
